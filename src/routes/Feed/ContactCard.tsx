4026613fb15578626be8b297a4189ade17c640b6
mizzu-creations <mizzu.creations@gmail.com>
2024-06-15 10:33:23 +0900

    Del : Linkedin

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -23,16 +23,6 @@ const ContactCard: React.FC = () => {
             <div className="name">github</div>
           </a>
         )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillLinkedin className="icon" />
-            <div className="name">linkedin</div>
-          </a>
-        )}
       </StyledWrapper>
     </>
   )
